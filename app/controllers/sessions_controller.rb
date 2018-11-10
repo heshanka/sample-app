32a30e549d71beb46d686b8bef3fba70dcb509f1
heshanka <heshanka123@gmail.com>
2018-11-10 19:06:01 +0530

    Added dropdown: account for successful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,14 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
-    user = User.find_by(email: params[:session] [:email].downcase)
-    if user && user.authenticate(params[:session] [:password])
-      #log the user in and direct to show user's page
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to user
     else
-      #create an error message
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
