1dd4bca8486a648550abb3ebd173fb190a572860
heshanka <heshanka123@gmail.com>
2018-11-14 14:41:26 +0530

    Implemented basic login - completed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,8 +2,7 @@ module UsersHelper
   def gravatar_for(user, options = {size: 80} )
   #Returns the gravatar for the given user
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s={size}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
