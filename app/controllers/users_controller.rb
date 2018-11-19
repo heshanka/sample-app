391de3d29796ecd02d10534b152417d397a32692
heshanka <heshanka123@gmail.com>
2018-11-19 21:22:51 +0530

    Users index created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,8 +20,16 @@ class UsersController < ApplicationController
     end
   end
 
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 
   def edit
   end
